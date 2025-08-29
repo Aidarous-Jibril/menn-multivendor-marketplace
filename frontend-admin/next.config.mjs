3acf2e60f51a235782d5406836e6a66c157892e1
Aidarous Jibril <aidarousj7@gmail.com>
2025-08-29 19:36:12 +0200

    Update next.config.mjs

--- frontend-admin/next.config.mjs
+++ frontend-admin/next.config.mjs
@@ -1,18 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//    remotePatterns: [
-//       { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
-//    ]
-//   },
-// };
-
-// export default nextConfig;
-
-
-
-
 /** @type {import('next').NextConfig} */
 const NEXT_API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
