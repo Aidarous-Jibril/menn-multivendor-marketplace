f60dbd49dc8a40ed06f78fb46a653f4f99f46ae7
Aidarous Jibril <aidarousj7@gmail.com>
2025-08-29 19:35:53 +0200

    Update axiosInstance.js

--- frontend-admin/utils/axiosInstance.js
+++ frontend-admin/utils/axiosInstance.js
@@ -1,14 +1,3 @@
-// // utils/axiosInstance.js
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_API_URL, // e.g. http://localhost:8000
-//   withCredentials: true,
-//   headers: {  "Content-Type": "application/json" },
-// });
-
-// export default axiosInstance;
-
 
 import axios from "axios";
 
